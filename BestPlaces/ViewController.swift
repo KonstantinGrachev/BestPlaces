d294f06f174fcb7eb424bdc54343cc8127f26d21
Konstantin Grachev <91027381+KonstantinGrachev@users.noreply.github.com>
2022-09-07 12:26:53 +0300

    Initial Commit

---
+++ BestPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestPlaces
+//
+//  Created by Konstantin Gracheff on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
