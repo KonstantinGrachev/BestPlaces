4955c94993e42530f5087848212f43dd3edd4900
Konstantin Grachev <91027381+KonstantinGrachev@users.noreply.github.com>
2022-09-08 22:14:46 +0300

    icons + constants
    add constants
    add custom icons

--- BestPlaces/ViewController.swift
+++ BestPlaces/ViewController.swift
@@ -1,14 +1,11 @@
-//
-//  ViewController.swift
-//  BestPlaces
-//
-//  Created by Konstantin Gracheff on 07.09.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum Constants {
+        static let heightForRow: CGFloat = 85
+    }
+    
     private let placesTableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -34,7 +31,7 @@ class ViewController: UIViewController {
         placesTableView.dataSource = self
         placesTableView.register(PlaceCell.self, forCellReuseIdentifier: PlaceCell.placeCellId)
     }
-
+    
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -43,9 +40,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        50
+        Constants.heightForRow
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = placesTableView.dequeueReusableCell(withIdentifier: PlaceCell.placeCellId, for: indexPath) as? PlaceCell else { return UITableViewCell() }
         cell.configure(textLabel: placesArray[indexPath.row],
